4388ff85659c01d6c01f60d2018d97133a68f3db
mliboz <maxoulib@gmail.com>
2024-01-14 18:30:00 +0100

    Evo: using HashRouter for github refresh to work

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	<BrowserRouter basename={process.env.PUBLIC_URL}>
+	<HashRouter basename={process.env.PUBLIC_URL}>
 		<Routes>
 			<Route path="/*" element={<App />} />
 		</Routes>
-	</BrowserRouter>
+	</HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
